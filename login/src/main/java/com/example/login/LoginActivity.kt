e83b87efccdc41bbbe0b6c2d6172c7e60e9e5d90
zhujie <zhujie@damiyin.com>
2019-09-11 16:38:45 +0800

    新增登录模块

---
+++ login/src/main/java/com/example/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_main)
+    }
+}
